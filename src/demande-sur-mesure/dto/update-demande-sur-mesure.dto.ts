37c3acbf3339cec05c76ba8fe022ac612b3e8c67
adamos6666 <youssef.karoui68@gmail.com>
2025-07-26 18:14:19 +0100

    Fix dependencies and add missing packages for JWT auth

--- src/demande-sur-mesure/dto/update-demande-sur-mesure.dto.ts
+++ src/demande-sur-mesure/dto/update-demande-sur-mesure.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateDemandeSurMesureDto } from './create-demande-sur-mesure.dto';
-
+ 
 export class UpdateDemandeSurMesureDto extends PartialType(CreateDemandeSurMesureDto) {} 
